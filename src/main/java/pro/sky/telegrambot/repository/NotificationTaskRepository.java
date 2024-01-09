97341fe8fcc27259aa46e4c78b7223c631427f6e
Ксения <korzhuk.xeniya@yandex.ru>
2024-01-09 20:20:30 +0300

    problem!!

--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -6,8 +6,9 @@ import pro.sky.telegrambot.model.NotificationTask;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+public interface NotificationTaskRepository extends JpaRepository<NotificationTask, UUID> {
 //    Optional<NotificationTask> saveById(Long Id);
     List<NotificationTask> findByAlarmDate(LocalDateTime alarmDate);
 
