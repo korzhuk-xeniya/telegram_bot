e88bba42875459be197d5615a4c9d3c870758af5
Ксения <korzhuk.xeniya@yandex.ru>
2024-01-09 20:14:00 +0300

    problem!

--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -3,9 +3,12 @@ package pro.sky.telegrambot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.sky.telegrambot.model.NotificationTask;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
-    Optional<NotificationTask> saveById(Long Id);
+//    Optional<NotificationTask> saveById(Long Id);
+    List<NotificationTask> findByAlarmDate(LocalDateTime alarmDate);
 
 }
--- src/main/java/pro/sky/telegrambot/service/NotificationServiseImpl.java
+++ src/main/java/pro/sky/telegrambot/service/NotificationServiseImpl.java
@@ -3,20 +3,38 @@ package pro.sky.telegrambot.service;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.model.Update;
+import org.slf4j.LoggerFactory;
+import pro.sky.telegrambot.repository.NotificationTaskRepository;
+
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 
 public abstract class NotificationServiseImpl implements NotificationServise {
 
     private TelegramBot telegramBot;
+    private NotificationTaskRepository repository;
+    private Logger logger = (Logger) LoggerFactory.getLogger(NotificationServiseImpl.class);
+    private static Pattern MESSAGE_PATTERN = Pattern.compile("([0-9\\.:\\s]{16})(\\s)(.+)");
+    private  static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy hh:mm");
     @Override
     public void process(Update update) {
 
+
+        if (update.message() == null) {
+            logger.info("пользователь отправил пустое сообщение");
+            return;
+        }
         Long chatId = update.message().chat().id();
         String message = update.message().text();
+        if (message==null) {
+            telegramBot.execute(new SendMessage(chatId, "для начала работы, отправь /start"));
+            return;
+        }
         if (message.equals("/start")) {
-
             sendWelcomMessage(chatId);
-
+            return;
 }
     }
 
