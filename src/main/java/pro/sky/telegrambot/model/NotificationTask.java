d36d7e3dcb81aae2fe2ca05c0e11dcc582b4143d
Ксения <korzhuk.xeniya@yandex.ru>
2024-01-18 21:51:58 +0300

    problem with data format

--- src/main/java/pro/sky/telegrambot/model/NotificationTask.java
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 
 public class NotificationTask {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
--- src/main/java/pro/sky/telegrambot/service/NotificationServiseImpl.java
+++ src/main/java/pro/sky/telegrambot/service/NotificationServiseImpl.java
@@ -28,7 +28,8 @@ public  class NotificationServiseImpl implements NotificationServise {
     private final TelegramBot telegramBot;
     private final NotificationTaskRepository repository;
     private final Logger logger =  LoggerFactory.getLogger(NotificationServiseImpl.class);
-    private static final Pattern MESSAGE_PATTERN = Pattern.compile("([0-9\\.:\\s]{16})(\\s)(.+)");
+//    private static final Pattern MESSAGE_PATTERN = Pattern.compile("([0-9\\.:\\s]{16})(\\s)(.+)");
+    private static final Pattern MESSAGE_PATTERN = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy hh:mm");
 
     public NotificationServiseImpl(TelegramBot telegramBot, NotificationTaskRepository repository) {
